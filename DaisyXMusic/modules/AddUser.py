27f43a1abf943bd0b62207a81550a02398a06107
Ferlyk19 <87972799+Ferlyk19@users.noreply.github.com>
2021-08-21 16:23:25 +0700

    Update AddUser.py

--- DaisyXMusic/modules/AddUser.py
+++ DaisyXMusic/modules/AddUser.py
@@ -4,7 +4,7 @@ from os import path
 from typing import Callable
 form DaisyXMusic.modules import play
 
-authorized_channel_only = CHANNEL_USER
+authorized_channel_only = {CHANNEL_UPDATES}
 CHANNEL_USER = https://t.me/Channelrumah
 
 @Client.on_message(command("play") & other_filters)
