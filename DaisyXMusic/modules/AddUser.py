d22cc49c44b6d24686dc397bad96229ec1daac10
Ferlyk19 <87972799+Ferlyk19@users.noreply.github.com>
2021-08-21 16:53:25 +0700

    Update AddUser.py

--- DaisyXMusic/modules/AddUser.py
+++ DaisyXMusic/modules/AddUser.py
@@ -4,15 +4,15 @@ from os import path ;
 from typing import Callable ;
 form DaisyXMusic.modules import play ;
 
-authorized_channel_only = {UPDATES_CHANNEL};
+authorized_users_only = {UPDATES_CHANNEL};
 id_user = message.chat.id
 {
-@Client.on_message(command.handler("play|ytplay|start|skip|end|userbotjoin|") & other_filters) ;
+@Client.on_message(commandHandler("play|ytplay|start|skip|end|userbotjoin|") & other_filters) ;
 def action_start(message) ;
     user = open('user.txt,'r') ;
     user = user.read () ;
     Id_message = message.chat.id ;
-    if str (id_message) in {UPDATES_CHANNEL} :
+    if str(id_message) in {UPDATES_CHANNEL} :
            worker : import play
     else:
          bot.reply_to(message, 'hanya subscriber yang dapat mengakses saya')
