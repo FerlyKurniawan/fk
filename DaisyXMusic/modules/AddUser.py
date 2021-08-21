bcde22938df5055c70f119c3f8e06af9238e6a6d
Ferlyk19 <87972799+Ferlyk19@users.noreply.github.com>
2021-08-21 17:22:53 +0700

    Update AddUser.py

--- DaisyXMusic/modules/AddUser.py
+++ DaisyXMusic/modules/AddUser.py
@@ -4,16 +4,16 @@ from os import path
 from typing import Callable
 form DaisyXMusic.modules import play
 
-authorized_users_only = {UPDATES_CHANNEL};
-id_user = message.chat.id ;
+authorized_users_only = {UPDATES_CHANNEL}
+id_user = message.chat.id
 {
-@Client.on_message(MessageHandler("play|ytplay|start|skip|end|userbotjoin|") & other_filters) ;
-def action_start(message) ;
-    user = open('user.txt,'r') ;
-    user = user.read () ;
-    Id_message = message.chat.id ;
-    if str(id_message) in {UPDATES_CHANNEL} :
+@Client.on_message(MessageHandler("play|ytplay|start|skip|end|userbotjoin|") & other_filters)
+def action_start(message)
+    user = open('user.txt,'r')
+    user = user.read ()
+    Id_message = message.chat.id
+    if str(id_message) in {UPDATES_CHANNEL}
            worker : import play
     else:
-         bot.reply_to(message, 'hanya subscriber yang dapat mengakses saya') ;
+         bot.reply_to(message, 'hanya subscriber yang dapat mengakses saya')
 }
