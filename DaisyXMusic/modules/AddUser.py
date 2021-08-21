e93de46b89607268af9181dd98e9a851356494cc
Ferlyk19 <87972799+Ferlyk19@users.noreply.github.com>
2021-08-21 15:26:15 +0700

    Update AddUser.py

--- DaisyXMusic/modules/AddUser.py
+++ DaisyXMusic/modules/AddUser.py
@@ -1,1 +1,10 @@
+import json
+import os
+from os import path
+from typing import Callable
+form DaisyXMusic.modules import play
+
+authorized_channel_only = CHANNEL_USER
+CHANNEL_USER = https://t.me/Channelrumah
+
 
