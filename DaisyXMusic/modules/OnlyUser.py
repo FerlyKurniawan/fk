ca0d3bb5a1934f3e9ccecf446e5e130a8c97981d
Ferlyk19 <87972799+Ferlyk19@users.noreply.github.com>
2021-08-21 07:46:05 +0700

    Update OnlyUser.py

--- DaisyXMusic/modules/OnlyUser.py
+++ DaisyXMusic/modules/OnlyUser.py
@@ -2,3 +2,17 @@
 import json
 import os
 import play
+from os import path
+from typing import Callable
+
+id_user = message.chat.id
+
+@bot.message_handler(commands=['play'])
+def action_start(message)
+    user = open('user.txt','r')
+    user = user.read ()
+    Id_message = message_chat_id
+    if str (id_message) in channel user :
+           import play
+    else:
+           bot.reply_to(message, 'authorized_users_only')
